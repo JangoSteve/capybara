97bb7de0b777be6463ee966313d7f8ee8ca68f97
Steve Schwartz <steve@alfajango.com>
2011-10-06 22:13:26 -0400

    Updated to pay attention to cofig.assets.digest

--- lib/capybara/util/save_and_open_page.rb
+++ lib/capybara/util/save_and_open_page.rb
@@ -31,6 +31,7 @@ module Capybara
 
     def rewrite_css_and_image_references(response_html) # :nodoc:
       root = Capybara.asset_root
+      puts "Root: #{Capybara.asset_root}"
       return response_html unless root
       entries = Dir.new(root).entries
       directories = []
@@ -54,14 +55,15 @@ module Capybara
     def rewrite_assets_from_asset_pipeline(response_html)
       asset_prefix = ::Rails.application.config.assets.prefix
       public_asset_path = File.join(::Rails.public_path, asset_prefix)
-      manifest = File.join(public_asset_path, "manifest.yml")
+      manifest = File.join(::Rails.application.config.assets.manifest, "manifest.yml")
       unless Dir.exists?(public_asset_path) && File.exists?(manifest)
         raise "You must first run `rake assets:precompile`"
       end
-      assets    = YAML::load(File.open(manifest))
+      assets = YAML::load(File.open(manifest))
       assets.each do |name, asset_name|
         response_html.gsub!(name, asset_name)
       end
+
       return response_html
     end
   end
